b3434aabf2bdc69af34f0b4157bf23ea398ce3a5
lucasfonte21 <lucasfonte@ufl.edu>
2026-08-21 12:14:43 -0400

    Connected the frontend and backend with a test API route

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'hello from server' });
+});
 
 //start server
 app.listen(PORT, () => {
